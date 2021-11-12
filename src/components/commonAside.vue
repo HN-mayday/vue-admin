1386df0019a444420354dbe3be45e29fd518189f
siwie <2760595805@qq.com>
2021-11-12 13:24:30 +0800

    用户管理系统

---
+++ src/components/commonAside.vue
@@ -0,0 +1,128 @@
+<template>
+    <el-menu  class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="ffd04b"
+    >
+        <h3> 通用后台管理系统</h3>
+          <el-menu-item :index="item.path" v-for="item in noChildren" :key="item.path"
+          @click="clickMenu(item)"
+          >
+    <i :class="'el-icon-'+ item.icon"></i>
+    <span slot="title">{{ item.lable }}</span>
+  </el-menu-item>
+  <el-submenu :index="item.lable" 
+  v-for="item in hasChildren" 
+  :key="item.path"
+  >
+    <template slot="title">
+      <i :class="'el-icon'+ item.icon"></i>
+      <span slot="title"> {{ item.lable }}</span>
+    </template>
+    <el-menu-item-group>
+      <el-menu-item 
+      :index="subItem.path"
+       v-for="(subItem, subIndex) in item.children"
+        :key="subIndex"
+        @click="clickMenu(item)">
+         <i :class="'el-icon-'+ subItem.icon"></i>
+      <span slot="title"> {{ subItem.label }}</span>
+          </el-menu-item>
+    </el-menu-item-group>
+  </el-submenu>
+</el-menu>
+</template>
+<style>
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        isCollapse: false,
+         menu: [
+        {
+          path: "/",
+          name: "home",
+          label: "首页",
+          icon: "s-home",
+          url: "Home/Home",
+        },
+        {
+          path: "/mall",
+          name: "mall",
+          label: "商品管理",
+          icon: "video-play",
+          url: "MallManage/MallManage",
+        },
+        {
+          path: "/user",
+          name: "user",
+          label: "用户管理",
+          icon: "user",
+          url: "UserManage/UserManage",
+        },
+        {
+          label: "其他",
+          icon: "location",
+          children: [
+            {
+              path: "/page1",
+              name: "page1",
+              label: "页面1",
+              icon: "setting",
+              url: "Other/PageOne",
+            },
+            {
+              path: "/page2",
+              name: "page2",
+              label: "页面2",
+              icon: "setting",
+              url: "Other/PageTwo",
+            },
+          ],
+        },
+      ],
+    };
+  },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      clickMenu(item) {
+          this.$router.push({ name: item.name});
+      },
+    },
+    computed: { 
+    noChildren() {
+      return this.menu.filter((item) => !item.children);
+    },
+    hasChildren() {
+      return this.menu.filter((item) => item.children);
+    },
+  },
+  };
+  
+</script>
+<style lang="scss" scoped>
+.el-menu {
+  height: 100%;
+  border: none;
+  h3 {
+    color: #ffffff;
+    text-align: center;
+    line-height: 48px;
+  }
+}
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-container style="height: 100%">
+  <el-aside width="auto"><common-aside></common-aside></el-aside>
+  <el-container>
+    <el-header>
+      <common-header></common-header>
+    </el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+</template>
+
+<script>
+import CommonAside from '../components/commonAside';
+import CommonHeader from '../components/CommonHeader';
+
+export default{
+  name: 'Home',
+  components: { CommonAside ,CommonHeader},
+    
+};
+
+</script>
+<style lang="css" scoped>
+.el-header {
+  background-color: #333;
+}
+.el-main {
+  padding-top: 0;
+}
+  
+</style>
